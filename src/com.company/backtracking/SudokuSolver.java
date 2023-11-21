deefc4a23ab90939b3f0a9fb93fae407380540ab
AHarishma <harishmakiran@gmail.com>
2023-11-21 16:21:37 +0530

    Add Sudoku solver, unique paths, palindrome partitioning

--- src/com.company/backtracking/SudokuSolver.java
+++ src/com.company/backtracking/SudokuSolver.java
@@ -1,25 +1,25 @@
 package backtracking;
 
 public class SudokuSolver {
-    public boolean solveSudoku(int[][] board) {
+    private boolean solveSudoku(int[][] board) {
         return canBeSolved(board, 0, 0);
     }
 
     private boolean canBeSolved(int[][] board, int row, int col) {
-        if(row == board.length) {
+        if (row == board.length) {
             return true;
         }
-        if(col == board[row].length) {
+        if (col == board[row].length) {
             return canBeSolved(board, row + 1, 0);
         }
-        if(board[row][col] != 0) {
+        if (board[row][col] != 0) {
             return canBeSolved(board, row, col + 1);
         }
 
-        for(int i = 1;  i<=9; i++) {
-            if(isValidNumber(board, row, col, i)) {
+        for (int i = 1; i <= 9; i++) {
+            if (isValidNumber(board, row, col, i)) {
                 board[row][col] = i;
-                if(canBeSolved(board, row, col + 1)) {
+                if (canBeSolved(board, row, col + 1)) {
                     return true;
                 }
                 board[row][col] = 0;
@@ -29,28 +29,29 @@ public class SudokuSolver {
     }
 
     private boolean isValidNumber(int[][] board, int row, int col, int number) {
-        for(int i = 0; i< board.length; i++) {
-            if(board[row][i] == number) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] == number) {
                 return false;
             }
         }
-        for(int i = 0; i< board.length; i++) {
-            if(board[i][col] == number) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col] == number) {
                 return false;
             }
         }
 
         int subGridRow = (row / 3) * 3;
         int subGridCol = (col / 3) * 3;
-        for(int i = subGridRow;  i< subGridRow + 3; i++) {
-            for(int j = subGridCol;  j< subGridCol + 3; j++) {
-                if(board[i][j] == number) {
+        for (int i = subGridRow; i < subGridRow + 3; i++) {
+            for (int j = subGridCol; j < subGridCol + 3; j++) {
+                if (board[i][j] == number) {
                     return false;
                 }
             }
         }
-        return  true;
+        return true;
     }
+
     private static void printBoard(int[][] board) {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
