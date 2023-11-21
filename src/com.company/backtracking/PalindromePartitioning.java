f96c9acb1d8d45e00e12b1310188901deb12fe78
AHarishma <harishmakiran@gmail.com>
2023-11-21 16:21:37 +0530

    Add Sudoku solver, unique paths, palindrome partitioning

---
+++ src/com.company/backtracking/PalindromePartitioning.java
@@ -0,0 +1,38 @@
+package backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PalindromePartitioning {
+
+    void palindromeParitioning(String string, int index, List<String> currentList, List<List<String>> result) {
+        if(index >= string.length()) {
+            result.add(new ArrayList<>(currentList));
+            return;
+        }
+
+        for(int i = index; i< string.length(); i++) {
+            if(isPalindrome(string.substring(index, i + 1))) {
+                currentList.add(string.substring(index, i + 1));
+                palindromeParitioning(string, index + 1, currentList, result);
+                currentList.remove(currentList.size() - 1);
+            }
+        }
+    }
+
+    boolean isPalindrome(String string) {
+        StringBuilder builder = new StringBuilder(string).reverse();
+        return string.equals(builder.toString());
+    }
+    public static void main(String[] args) {
+        List<List<String>> result = new ArrayList<>();
+        new PalindromePartitioning().palindromeParitioning("aabbc", 0, new ArrayList<>(), result);
+        for(int i= 0; i< result.size(); i++) {
+            List<String> cur = result.get(i);
+            for(int j = 0; j < cur.size(); j++) {
+                System.out.print(cur.get(j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/com.company/backtracking/UniquePaths.java
@@ -0,0 +1,74 @@
+package backtracking;
+
+import java.util.*;
+
+public class UniquePaths {
+
+    int[] rowIndex =  new int[] {-1, 0, 0, 1};
+    int[] colIndex = new int[]{0, -1, 1, 0};
+    static int count = 0;
+    private int solve(int[][] maze) {
+        int[] start = new int[] {-1, -1};
+        for(int i = 0; i< maze.length; i++){
+            for(int j = 0; j< maze[0].length; j++) {
+                if(maze[i][j] == 1) {
+                    start[0] = i;
+                    start[1] = j;
+                    break;
+                }
+            }
+            if(start[0] != -1) {
+                break;
+            }
+        }
+        if(start[0] == -1) {
+            return 0;
+        }
+        boolean[][] visited = new boolean[maze.length][maze[0].length];
+        Arrays.stream(visited).forEach(array ->  Arrays.fill(array, false));
+        visited[start[0]][start[1]] = true;
+        dfs(maze, start[0], start[1], visited);
+        return count;
+    }
+
+    void dfs(int[][] maze, int i, int j, boolean[][] visited) {
+        if(maze[i][j] == 2) {
+            if(isAllVisited(maze, visited)) {
+                count++;
+            }
+            return;
+        }
+
+        for(int k = 0; k < rowIndex.length; k++) {
+            int row = i + rowIndex[k];
+            int col = j + colIndex[k];
+            if(row>=0 &&  row < maze.length && col >= 0 && col < maze[row].length &&
+                    (maze[row][col] == 0 || maze[row][col] == 2) && !visited[row][col]) {
+                visited[row][col] = true;
+                dfs(maze, row, col, visited);
+                visited[row][col] = false;
+            }
+        }
+    }
+
+    boolean isAllVisited(int[][] maze, boolean[][] visited)  {
+        for(int i = 0; i< maze.length; i++) {
+            for(int j = 0; j< maze[0].length; j++) {
+                if(maze[i][j] == 0 && !visited[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[][] maze = {
+            {1,0},
+            {2,0}
+        };
+        UniquePaths uniquePaths = new UniquePaths();
+        uniquePaths.solve(maze);
+        System.out.println("No of unique paths ::  " + count);
+    }
+}
