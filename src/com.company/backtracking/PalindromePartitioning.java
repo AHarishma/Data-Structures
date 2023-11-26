b652f6eeecc579d2465ef1c351742fff6ff7055a
AHarishma <harishmakiran@gmail.com>
2023-11-27 00:05:34 +0530

    Add heap and n queens problem

--- src/com.company/backtracking/PalindromePartitioning.java
+++ src/com.company/backtracking/PalindromePartitioning.java
@@ -50,49 +50,49 @@ public class PalindromePartitioning {
 /**
  * Problem Description
  * Given a string A, partition A such that every string of the partition is a palindrome.
- *
+ * <p>
  * Return all possible palindrome partitioning of A.
- *
+ * <p>
  * Ordering the results in the answer : Entry i will come before Entry j if :
  * len(Entryi[0]) < len(Entryj[0]) OR
  * (len(Entryi[0]) == len(Entryj[0]) AND len(Entryi[1]) < len(Entryj[1])) OR * * *
  * (len(Entryi[0]) == len(Entryj[0]) AND ... len(Entryi[k] < len(Entryj[k]))
- *
+ * <p>
  * Problem Constraints
  * 1 <= len(A) <= 15
- *
+ * <p>
  * Input Format
  * First argument is a string A of lowercase characters.
- *
+ * <p>
  * Output Format
  * Return a list of all possible palindrome partitioning of s.
- *
+ * <p>
  * Example Input
  * Input 1:
- *
+ * <p>
  * A = "aab"
  * Input 2:
  * <p>
  * A = "a"
- *
+ * <p>
  * Example Output
  * Output 1:
- *
+ * <p>
  * [
  * ["a","a","b"]
  * ["aa","b"],
  * ]
  * Output 2:
- *
+ * <p>
  * [
  * ["a"]
  * ]
- *
+ * <p>
  * Example Explanation
  * Explanation 1:
- *
+ * <p>
  * In the given example, ["a", "a", "b"] comes before ["aa", "b"] because len("a") < len("aa").
  * Explanation 2:
- *
+ * <p>
  * In the given example, only partition possible is "a" .
  */
