8617b8e1613235ddf7eaa0130e37fa641e0ca814
AHarishma <harishmakiran@gmail.com>
2023-12-08 21:34:07 +0530

    Add merge k sorted list

---
+++ src/main/java/com/company/linkedlist/ListNode.java
@@ -0,0 +1,29 @@
+package com.company.linkedlist;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public static boolean areEqual(ListNode head1, ListNode head2) {
+        while (head1 != null && head2 != null) {
+            if (head1.val != head2.val) {
+                return false;
+            }
+            head1 = head1.next;
+            head2 = head2.next;
+        }
+        return head1 == null && head2 == null;
+    }
+}
