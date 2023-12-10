d9897577f07a72a374b25a7d4ecbacbd50755649
AHarishma <harishmakiran@gmail.com>
2023-12-11 00:06:48 +0530

    Add single number three

---
+++ src/main/java/com.company/bitmanipulation/SingleNumberThree.java
@@ -0,0 +1,26 @@
+package com.company.bitmanipulation;
+
+public class SingleNumberThree {
+    public int[] singleNumber(int[] nums) {
+        int xor = 0;
+        for (int i = 0; i < nums.length; i++) {
+            xor ^= nums[i];
+        }
+        int setBitPos = -1;
+        for (int i = 0; i < 31; i++) {
+            if (((xor >> i) & 1) == 1) {
+                setBitPos = i;
+            }
+        }
+        int bag1Xor = 0;
+        int bag2Xor = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (((nums[i] >> setBitPos) & 1) == 1) {
+                bag1Xor ^= nums[i];
+            } else {
+                bag2Xor ^= nums[i];
+            }
+        }
+        return new int[]{bag1Xor, bag2Xor};
+    }
+}
