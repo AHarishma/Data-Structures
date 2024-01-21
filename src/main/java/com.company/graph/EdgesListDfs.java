7b184a652adcb287f7e5fa78a1070ab69c37a0be
AHarishma <harishmakiran@gmail.com>
2024-01-21 06:54:53 +0530

    Add test for edges list dfs

--- src/main/java/com.company/graph/EdgesListDfs.java
+++ src/main/java/com.company/graph/EdgesListDfs.java
@@ -6,24 +6,27 @@ import java.util.List;
 import java.util.Map;
 
 public class EdgesListDfs {
-    public void depthFirstSearch(int[][] edges, int n) {
+    public List<Integer> depthFirstSearch(int[][] edges, int n) {
         Map<Integer, List<Integer>> graph = generateGraph(edges);
+        List<Integer> vertices = new ArrayList<>();
         boolean[] visited = new boolean[n];
-        dfs(graph, 0, visited);
+        dfs(graph, 0, visited, vertices);
+        return vertices;
     }
 
-    private void dfs(Map<Integer, List<Integer>> graph, int vertex, boolean[] visited) {
+    private void dfs(Map<Integer, List<Integer>> graph, int vertex, boolean[] visited, List<Integer> vertices) {
         visited[vertex] = true;
-        for(int adjacent : graph.getOrDefault(vertex, new ArrayList<>())) {
-            if(!visited[adjacent]) {
-                dfs(graph, adjacent, visited);
+        vertices.add(vertex);
+        for (int adjacent : graph.getOrDefault(vertex, new ArrayList<>())) {
+            if (!visited[adjacent]) {
+                dfs(graph, adjacent, visited, vertices);
             }
         }
     }
 
     private Map<Integer, List<Integer>> generateGraph(int[][] edges) {
         Map<Integer, List<Integer>> graph = new HashMap<>();
-        for(int[] edge: edges) {
+        for (int[] edge : edges) {
             graph.putIfAbsent(edge[0], new ArrayList<>());
             graph.get(edge[0]).add(edge[1]);
         }
