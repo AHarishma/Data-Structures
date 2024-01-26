d901b1f24e9b3f5d84e47e266e30ad17606d0140
AHarishma <harishmakiran@gmail.com>
2024-01-27 00:05:06 +0530

    remove extra space

--- src/main/java/com.company/linkedlist/ReverseBetween.java
+++ src/main/java/com.company/linkedlist/ReverseBetween.java
@@ -1,18 +1,18 @@
 package com.company.linkedlist;
 
 public class ReverseBetween {
-    public  ListNode reverseBetween( ListNode head, int m, int n) {
+    public ListNode reverseBetween( ListNode head, int m, int n) {
         if (head == null || m == n) {
             return head;
         }
-         ListNode dummy = new  ListNode(0);
+        ListNode dummy = new  ListNode(0);
         dummy.next = head;
-         ListNode prev = dummy;
+        ListNode prev = dummy;
         for (int i = 1; i < m; i++) {
             prev = prev.next;
         }
-         ListNode current = prev.next;
-         ListNode tail = current;
+        ListNode current = prev.next;
+        ListNode tail = current;
         for(int i = m; i <= n; i++) {
             ListNode nextNode = current.next;
             current.next = prev.next;
