a8346b3403d9ab99160fbb15e15ef502a784f6b1
AHarishma <harishmakiran@gmail.com>
2024-01-27 14:29:34 +0530

    sort stack

--- src/test/java/com/company/stack/MergeInAscendingOrderTest.java
+++ src/test/java/com/company/stack/MergeInAscendingOrderTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Stack;
 import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MergeInAscendingOrderTest {
